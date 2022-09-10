b0173d81d8a3f248acb793dd73895a52d20462ed
NicoRondonData <nrondon@ucompensar.edu.co>
2022-09-10 10:51:45 -0500

    blog

--- todo/django_project/settings.py
+++ todo/django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "corsheaders",
     "todos"
 ]
 
@@ -51,6 +52,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = (
+"http://localhost:3000",
+"http://localhost:8000",
+)
 ROOT_URLCONF = 'django_project.urls'
 
 TEMPLATES = [
